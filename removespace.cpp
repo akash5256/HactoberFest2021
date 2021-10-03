e1e4c3401a47b05ebd044e2bcbd033fcd3f29267
Akash kumar maurya <71809598+akash5256@users.noreply.github.com>
2021-10-03 17:42:11 +0530

    Create removespace.cpp

---
+++ removespace.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+
+int main()
+{
+    std::string str = "H e l l o";
+    str.erase(remove(str.begin(), str.end(), ' '), str.end());
+    std::cout << str; // Output Hello
+    
+    return 0;
+}
